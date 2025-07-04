a977d34f39b068a9cbd39f2d43e772e1c635cfa5
amrutha2508 <147953598+amrutha2508@users.noreply.github.com>
2025-07-04 07:35:15 -0400

    learned unittesting

---
+++ ETL/unittest/test_mymodule.py
@@ -0,0 +1,26 @@
+import unittest
+
+from mymodule import square, double, add
+
+class TestMyModule(unittest.TestCase):
+    def test_square(self):
+        self.assertEqual(square(2),4)
+        self.assertEqual(square(3.0), 9.0)
+        self.assertNotEqual(square(-3), -9)
+
+    def test_double(self):
+        self.assertEqual(double(3),6)
+        self.assertEqual(double(-3.1), -6.2)
+        self.assertEqual(double(0), 0)
+
+    def test_add(self):
+        self.assertEqual(add(2,4),6)
+        self.assertEqual(add(0,0),0)
+        self.assertEqual(add(2.3,3.6),5.9)
+        self.assertEqual(add('hello','world'),'helloworld')
+        self.assertEqual(add(2.300,4.300),6.6)
+        self.assertNotEqual(add(-2,-2),0)
+
+unittest.main()
+
+    
