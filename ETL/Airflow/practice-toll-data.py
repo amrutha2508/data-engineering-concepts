7e09439c46387a5c4299ed21669b59b46c748aa6
amrutha2508 <147953598+amrutha2508@users.noreply.github.com>
2025-07-20 08:57:23 -0400

    Update practice-toll-data.py

--- ETL/Airflow/practice-toll-data.py
+++ ETL/Airflow/practice-toll-data.py
@@ -19,8 +19,6 @@ from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
 
-
-
 dag_args = {
     "owner":"yourname",
     "start_date":days_ago(0),
@@ -33,63 +31,44 @@ dag_args = {
 
 dag = DAG(
     "ETL-toll-data",
-    schedule_interval = timedelta(day=1)
+    schedule_interval = timedelta(minutes=1),
     default_args=dag_args,
     description="Apache airflow assignment",
 )
 
-download_dataset = BashOperator(
-    task_id = "download_dataset",
-    bash_command = "wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Final%20Assignment/tolldata.tgz"
-    dag = dag
-)
-
-untar_dataset = BashOperator(
-    task_id = "untar_dataset",
-    bash_command = "tar -xzf tolldata.tgz",
-    dag = dag
-)
-    
 
 unzip_data = BashOperator(
     task_id = "unzip_data",
     bash_command = 'tar -xzf /home/project/airflow/dags/finalassignment/tolldata.tgz -C /home/project/airflow/dags/finalassignment/staging/',
     dag=dag
 )
-
 extract_data_from_csv = BashOperator(
-    task_id = "extract_from_csv".
-    bash_command = "cut -d',' -f1,2,3,4 /home/project/airflow/dags/finalassignment/staging/vehicle-data.csv > /home/project/airflow/dags/finalassignment/staging/csv_data.csv"
+    task_id = "extract_from_csv",
+    bash_command = 'cut -d"," -f1,2,3,4 /home/project/airflow/dags/finalassignment/staging/vehicle-data.csv > /home/project/airflow/dags/finalassignment/staging/csv_data.csv',
     dag = dag 
 )
 
 extract_data_from_tsv = BashOperator(
-    task_id = "extract_from_tsv".
-    bash_command = "cut -f5-7 /home/project/airflow/dags/finalassignment/staging/tollplaza-data.tsv| tr '\t' ','  > /home/project/airflow/dags/finalassignment/staging/tsv_data.csv"
+    task_id = "extract_from_tsv",
+    bash_command = "cut -f5-7 /home/project/airflow/dags/finalassignment/staging/tollplaza-data.tsv| tr '\t' ','  > /home/project/airflow/dags/finalassignment/staging/tsv_data.csv",
     dag = dag
 )
 
 extract_data_from_fixed_width = BashOperator(
     task_id = "extract_from_fixed_width",
-    bash_command = "cat /home/project/airflow/dags/finalassignment/staging/payment-data.txt | tr -s ' ' | tr ' ' ',' | rev |cut -d',' -f1,2|rev > /home/project/airflow/dags/finalassignment/staging/fixed_width_data.csv"
+    bash_command = "cat /home/project/airflow/dags/finalassignment/staging/payment-data.txt | tr -s ' ' | tr ' ' ',' | rev |cut -d',' -f1,2|rev > /home/project/airflow/dags/finalassignment/staging/fixed_width_data.csv",
     dag = dag,
 )
 
 consolidate_data = BashOperator(
-    task = "consolidate_data",
+    task_id = "consolidate_data",
     bash_command = "paste -d',' \
         /home/project/airflow/dags/finalassignment/staging/csv_data.csv \
         /home/project/airflow/dags/finalassignment/staging/tsv_data.csv \
         /home/project/airflow/dags/finalassignment/staging/fixed_width_data.csv \
-        > /home/project/airflow/dags/finalassignment/staging/extracted_data.csv"
+        > /home/project/airflow/dags/finalassignment/staging/extracted_data.csv",
     dag = dag,
 )
-
-# transform_data = BashOperator(
-#     task = "transform_data",
-#     bash_command = ,
-#     dag = dag
-# )
 
 transform_data = BashOperator(
     task_id='transform_data',
@@ -104,10 +83,10 @@ transform_data = BashOperator(
         c4_upper=$(echo "$c4" | tr '[:lower:]' '[:upper:]')
         echo "$c1,$c2,$c3,$c4_upper,$c5,$c6,$c7,$c8,$c9" >> "$OUTPUT"
     done < "$INPUT"
-    """
+    """,
+    dag = dag
 )
-
-download_dataset >> untar_dataset >> unzip_data >> extract_data_from_csv >> extract_data_from_fixed_width >> extract_data_from_tsv >> consolidate_data >> transform_data
+unzip_data >> extract_data_from_csv >> extract_data_from_tsv >> extract_data_from_fixed_width >> consolidate_data >> transform_data
 
 
 # export AIRFLOW_HOME=/home/project/airflow
